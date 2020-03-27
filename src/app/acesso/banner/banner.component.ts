6db575693f78cefb711d28c5c0eed47b35c9b635
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-27 15:42:59 -0300

    Animate banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -24,9 +24,9 @@ export class BannerComponent implements OnInit {
   public estado = 'escondido';
 
   public imagens: Imagem[] = [
-    {estado: 'escondido', url: '/assets/banner-acesso/img_1.png'},
+    {estado: 'visivel', url: '/assets/banner-acesso/img_1.png'},
     {estado: 'escondido', url: '/assets/banner-acesso/img_2.png'},
-    {estado: 'visivel', url: '/assets/banner-acesso/img_3.png'},
+    {estado: 'escondido', url: '/assets/banner-acesso/img_3.png'},
     {estado: 'escondido', url: '/assets/banner-acesso/img_4.png'},
     {estado: 'escondido', url: '/assets/banner-acesso/img_5.png'}
   ];
@@ -38,7 +38,27 @@ export class BannerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    setTimeout(() => this.logicaRotacao(), 3000);
   }
 
+  public logicaRotacao() {
+    // Auxilia na exibição da imagem seguinte
+    let idx: number;
+
+    // Oculta uma imagem
+    for (let i = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido';
+
+        idx = i === 4 ? 0 : i + 1;
+
+        break;
+      }
+    }
+
+    // Exibe outra imagem do array
+    this.imagens[idx].estado = 'visivel';
+
+    setTimeout(() => this.logicaRotacao(), 3000);
+  }
 }
