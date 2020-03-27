60d84d5751db6dafd061050203215c65cb7ce2bb
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-27 15:20:03 -0300

    Model Imagem

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
+import { Imagem } from './imagem.model';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -19,15 +21,24 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class BannerComponent implements OnInit {
 
-  public estado: string = 'escondido';
+  public estado = 'escondido';
+
+  public imagens: Imagem[] = [
+    {estado: 'escondido', url: '/assets/banner-acesso/img_1.png'},
+    {estado: 'escondido', url: '/assets/banner-acesso/img_2.png'},
+    {estado: 'visivel', url: '/assets/banner-acesso/img_3.png'},
+    {estado: 'escondido', url: '/assets/banner-acesso/img_4.png'},
+    {estado: 'escondido', url: '/assets/banner-acesso/img_5.png'}
+  ];
 
   constructor() { }
 
   public toggleEstado(): void {
-    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'; 
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
   }
 
   ngOnInit(): void {
+
   }
 
 }
