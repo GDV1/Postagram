ced148e817e34cdaf8d961f1665c81eefb28a0af
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-27 15:08:46 -0300

    Creating animations e define Triggers

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
 
+  public estado: string = 'escondido';
+
   constructor() { }
+
+  public toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'; 
+  }
 
   ngOnInit(): void {
   }
