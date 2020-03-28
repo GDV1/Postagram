833bee27b53d7d3417cae5ff803721cccb9c91da
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-27 23:02:17 -0300

    Implemeting form control on Login component

--- src/app/acesso/login/login.component.ts
+++ src/app/acesso/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -12,11 +13,20 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(),
+    'senha': new FormControl(),
+  });
+
   ngOnInit(): void {
   }
 
   public exibirPainelCadastro(): void {
     this.exibirPainel.emit('cadastro');
+  }
+
+  public autenticar(): void {
+    console.log(this.formulario);
   }
 
 }
