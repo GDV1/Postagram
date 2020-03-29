2f2e6e8f3340569a7151e0ad19fd10b87e2b88b2
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-29 00:05:54 -0300

    Inserting new post on Firebase Storage (Image)

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -3,7 +3,13 @@ import * as firebase from 'firebase';
 export class BD {
     public publicar(publicacao: any): void {
         console.log(publicacao);
-        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-            .push({ titulo: publicacao.titulo, });
+
+        let nomeImagem = Date.now();
+
+        firebase.storage().ref().child(`imagens/${nomeImagem}`)
+        .put(publicacao.imagem);
+        
+        /*firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+            .push({ titulo: publicacao.titulo, });*/
     }
 }
